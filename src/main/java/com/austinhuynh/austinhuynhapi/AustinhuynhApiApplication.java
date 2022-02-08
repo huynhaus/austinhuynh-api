d88749509f5299fbbddb9d43f1165a5ee819bbd6
Austin Huynh <huynhaus@gmail.com>
2022-02-07 19:28:30 -0500

    Spring boot init

---
+++ src/main/java/com/austinhuynh/austinhuynhapi/AustinhuynhApiApplication.java
@@ -0,0 +1,13 @@
+package com.austinhuynh.austinhuynhapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AustinhuynhApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AustinhuynhApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/austinhuynh/austinhuynhapi/AustinhuynhApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.austinhuynh.austinhuynhapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AustinhuynhApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
